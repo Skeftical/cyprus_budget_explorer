bc8d02479d68e6923e9350b9daa2c0b8bb076855
Skeftical <fotissavva@hotmail.com>
2016-07-26 14:23:23 +0300

    finished populating the database

--- populate.py
+++ populate.py
@@ -10,80 +10,116 @@ django.setup()
 from visualisations.models import Office, SubOffice
 
 
-def add_office(id, name, year, total):
-    office = Office.objects.get_or_create(id=id, year=year, total=total)[0]
+def add_office(id, name, year, total, approved, revised):
+    office = Office.objects.get_or_create(officeId=id, year=year, total=total, approved=approved, revised=revised)[0]
     office.officeId = id
     office.name = name
     office.year = year
     office.total = total
+    office.approved = approved
+    office.revised = revised
     office.save()
     return office
 
 
-def add_subOffice_initial(office, subOfficeId, name, year, total):
-    subOffice = SubOffice.objects.get_or_create(office=office, subOfficeId=subOfficeId, year=year, total=total)[0]
+def add_subOffice_initial(office, subOfficeId, name, year, total, approved, revised):
+    subOffice = SubOffice.objects.get_or_create(office=office, subOfficeId=subOfficeId, year=year, total=total, approved=approved, revised=revised)[0]
     subOffice.name = name
     subOffice.year = year
     subOffice.total = total
+    subOffice.approved = approved
+    subOffice.revised = revised
     subOffice.save()
 
 
 def populate():
     # Only Offices and SubOffices
     firstline = True
+    firstrow = []
     with open('proupologismos-revised-csv.csv') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             if firstline:
                 firstline = False
+                firstrow = row
                 continue
             elif len(row[0]) == 2:
-                add_office(row[0], row[1], 2014, float(row[2]))
-            elif len(row[0]) == 4:
-                # SubOffices in here
+                #Add different years
+                i = 2
+                while i < len(row):
+                    year = int(firstrow[i][:4])
+                    if firstrow[i][-1]=='a':
+                        add_office(row[0], row[1], year, float(row[i]), True, False)
+                    elif firstrow[i][-1]=='r':
+                        add_office(row[0], row[1], year, float(row[i]), False, True)
+                    else:
+                        add_office(row[0], row[1], year, float(row[i]), False, False)
+
+                    i += 1
+            elif len(row[0]) == 4:  # SubOffices in here
                 officeId = row[0][:2]
                 subOfficeId = row[0]
                 name = row[1]
-                year = 2015
+                i = 2
                 total = float(row[2])
-                office = Office.objects.get(officeId=officeId)
-                add_subOffice_initial(office, subOfficeId, name, year, total)
-                # if len(row[0]) > 4 and not row[0] == "Column":
-                #     # Pagio, taktites, anaptuksiakes in here
+                while i < len(row):
+                    year = int(firstrow[i][:4])
+                    if firstrow[i][-1]=='a':
+                        office = Office.objects.get(officeId=officeId, year=year,approved=True,revised=False)
+                        add_subOffice_initial(office, subOfficeId, name, year, total, True, False)
+                    elif firstrow[i][-1]=='r':
+                        office = Office.objects.get(officeId=officeId, year=year,approved=False,revised=True)
+                        add_subOffice_initial(office, subOfficeId, name, year, total, False, True)
+                    else:
+                        office = Office.objects.get(officeId=officeId, year=year,approved=False,revised=False)
+                        add_subOffice_initial(office, subOfficeId, name, year, total, False, False)
+                    i += 1
+
+
 
 
 def add_more_data():
     firstline = True
+    firstrow = []
     with open('proupologismos-revised-csv.csv') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             if firstline:
                 firstline = False
+                firstrow = row
                 continue
             elif len(row[0]) > 4 and not row[0] == 'Column':
                 # SubOffices in here
                 subOfficeId = row[0][:4]
-                suboffice = SubOffice.objects.get(subOfficeId=subOfficeId)
-                if row[0][-1] == '1':
-                    # pagio
-                    pagio = float(row[2])
-                    suboffice.pagio = pagio
-                    suboffice.save()
-                elif row[0][-1] == '2':
-                    taktikes = float(row[2])
-                    suboffice.taktikes = taktikes
-                    suboffice.save()
-                elif row[0][-1] == '3':
-                    anaptuksiakes = float(row[2])
-                    suboffice.anaptuksiakes = anaptuksiakes
-                    suboffice.save()                    # anaptuksiakes
-                    # if len(row[0]) > 4 and not row[0] == "Column":
-                    #     # Pagio, taktites, anaptuksiakes in here
+                i=2
+                while i < len(row):
+                    year = int(firstrow[i][:4])
+                    if firstrow[i][-1]=='a':
+                        suboffice = SubOffice.objects.get(subOfficeId=subOfficeId, year=year,approved=True,revised=False)
+                    elif firstrow[i][-1]=='r':
+                        suboffice = SubOffice.objects.get(subOfficeId=subOfficeId, year=year,approved=False,revised=True)
+                    else:
+                        suboffice = SubOffice.objects.get(subOfficeId=subOfficeId, year=year,approved=False,revised=False)
+                    if row[0][-1] == '1':
+                        # pagio
+                        pagio = float(row[i])
+                        suboffice.pagio = pagio
+                        suboffice.save()
+                    elif row[0][-1] == '2':
+                        taktikes = float(row[i])
+                        suboffice.taktikes = taktikes
+                        suboffice.save()
+                    elif row[0][-1] == '3':
+                        anaptuksiakes = float(row[i])
+                        suboffice.anaptuksiakes = anaptuksiakes
+                        suboffice.save()  # anaptuksiakes
+                    i+=1
 
 
-#DIFFERENT YEARS
+# DIFFERENT YEARS
 
 if __name__ == '__main__':
     print "Starting Budget population script..."
     populate()
+    print "Finished initial population amending data..."
     add_more_data()
--- visualisations/models.py
+++ visualisations/models.py
@@ -8,9 +8,10 @@ from django.db import models
 class Office(models.Model):
     officeId = models.CharField(max_length=10)
     name = models.CharField(max_length=128)
-    year = models.IntegerField(null=True)
+    year = models.IntegerField()
     total = models.DecimalField(decimal_places=3, max_digits=18)
-
+    approved = models.BooleanField(default=False)
+    revised = models.BooleanField(default=False)
     def __unicode__(self):  #For Python 2, use __str__ on Python 3
         return self.name
 
@@ -24,8 +25,8 @@ class SubOffice(models.Model):
     pagio = models.DecimalField(decimal_places=3, max_digits=18, null=True)
     taktikes = models.DecimalField(decimal_places=3, max_digits=18, null=True)
     anaptuksiakes = models.DecimalField(decimal_places=3, max_digits=18, null=True)
-    approved = models.NullBooleanField(null=True)
-    revised = models.NullBooleanField(null=True)
+    approved = models.BooleanField(default=False)
+    revised = models.BooleanField(default=False)
 
     def __unicode__(self):  #For Python 2, use __str__ on Python 3
         return self.name
